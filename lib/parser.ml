9c58996ff16905c5c6b1d48d528d5d1ff54ee6e4
Matheus Henrique <matheuscdasilva2@hotmail.com>
2021-08-23 08:35:21 -0300

    fix: Get older parser combinators module version

--- lib/parser.ml
+++ lib/parser.ml
@@ -7,42 +7,24 @@ let make_input (s : string) : input = { text = s; pos = 0 }
 
 type error = { desc : string; pos : int }
 
-let make_error (desc : string) : error = { desc; pos = 0 }
+type 'a parser = { run : input -> (input * 'a, error) result }
 
-type 'a parser = { run : input -> input * ('a, string) result }
+let fail (e : error) = { run = (fun _ -> Error e) }
 
-let fail (e : string) = { run = (fun input -> (input, Error e)) }
-
-let wrap (x : 'a) = { run = (fun input -> (input, Ok x)) }
+let wrap (x : 'a) = { run = (fun input -> Ok (input, x)) }
 
 let map (f : 'a -> 'b) (p : 'a parser) : 'b parser =
   {
     run =
       (fun input ->
-        match p.run input with
-        | input', Ok x -> (input', Ok (f x))
-        | input', Error error -> (input', Error error));
+        match p.run input with Ok (input', x) -> Ok (input', f x) | Error error -> Error error);
   }
 
 let bind (f : 'a -> 'b parser) (p : 'a parser) : 'b parser =
   {
     run =
       (fun input ->
-        match p.run input with
-        | input', Ok x -> (f x).run input'
-        | input', Error error -> (input', Error error));
-  }
-
-let parse_n (m : int) (p : char -> bool) : string parser =
-  {
-    run =
-      (fun input ->
-        let n = String.length input.text in
-        let i = ref 0 in
-        while !i < m && !i < n && String.get input.text !i |> p do
-          incr i
-        done;
-        (input_sub !i (n - !i) input, Ok (String.sub input.text 0 !i)));
+        match p.run input with Ok (input', x) -> (f x).run input' | Error error -> Error error);
   }
 
 let parse_while (p : char -> bool) : string parser =
@@ -54,103 +36,104 @@ let parse_while (p : char -> bool) : string parser =
         while !i < n && String.get input.text !i |> p do
           incr i
         done;
-        (input_sub !i (n - !i) input, Ok (String.sub input.text 0 !i)));
+        Ok (input_sub !i (n - !i) input, String.sub input.text 0 !i));
+  }
+
+let parse_n (m : int) (p : char -> bool) : string parser =
+  {
+    run =
+      (fun input ->
+        let n = String.length input.text in
+        let i = ref 0 in
+        while !i < m && !i < n && String.get input.text !i |> p do
+          incr i
+        done;
+        Ok (input_sub !i (n - !i) input, String.sub input.text 0 !i));
   }
 
 let prefix (prefix_str : string) : string parser =
   {
     run =
       (fun input ->
-        let unexpected_prefix_error = Printf.sprintf "expected `%s`" prefix_str in
+        let unexpected_prefix_error =
+          { pos = input.pos; desc = Printf.sprintf "expected `%s`" prefix_str }
+        in
         try
           let prefix_size = String.length prefix_str in
           let input_size = String.length input.text in
           let prefix_input = input |> input_sub 0 prefix_size in
           if String.equal prefix_input.text prefix_str then
             let rest = input |> input_sub prefix_size (input_size - prefix_size) in
-            (rest, Ok prefix_str)
-          else (input, Error unexpected_prefix_error)
-        with Invalid_argument _ -> (input, Error unexpected_prefix_error));
+            Ok (rest, prefix_str)
+          else Error unexpected_prefix_error
+        with Invalid_argument _ -> Error unexpected_prefix_error);
   }
 
 let ( *> ) (p1 : 'a parser) (p2 : 'b parser) : 'b parser =
   {
     run =
-      (fun input ->
-        let input', result = p1.run input in
-        match result with Ok _ -> p2.run input' | Error e -> (input', Error e));
+      (fun input -> input |> p1.run |> Result.map (fun (input', _) -> p2.run input') |> Result.join);
   }
 
 let ( <* ) (p1 : 'a parser) (p2 : 'b parser) : 'a parser =
   {
     run =
       (fun input ->
-        let input', result = p1.run input in
-        match result with
-        | Ok x -> (
-            let input'', result' = p2.run input' in
-            match result' with Ok _ -> (input'', Ok x) | Error e -> (input'', Error e))
-        | Error e -> (input', Error e));
+        input
+        |> p1.run
+        |> Result.map (fun (input', x) ->
+               input' |> p2.run |> Result.map (fun (input, _) -> (input, x)))
+        |> Result.join);
   }
 
 let ( <*> ) (p1 : 'a parser) (p2 : 'b parser) : ('a * 'b) parser =
   {
     run =
       (fun input ->
-        let input', result = p1.run input in
-        match result with
-        | Ok x -> (
-            let input'', result' = p2.run input' in
-            match result' with Ok y -> (input'', Ok (x, y)) | Error e -> (input'', Error e))
-        | Error e -> (input', Error e));
+        input
+        |> p1.run
+        |> Result.map (fun (input', x) ->
+               input' |> p2.run |> Result.map (fun (input, y) -> (input, (x, y))))
+        |> Result.join);
   }
 
 let ( <|> ) (p1 : 'a parser) (p2 : 'a parser) : 'a parser =
   {
     run =
       (fun input ->
-        let input', result = p1.run input in
-        match result with Ok x -> (input', Ok x) | Error _left_error -> p2.run input);
+        match p1.run input with
+        | Ok (input', x) -> Ok (input', x)
+        | Error left_error ->
+            input
+            |> p2.run
+            |> Result.map_error (fun right_error ->
+                   {
+                     pos = left_error.pos;
+                     desc = Printf.sprintf "%s or %s" left_error.desc right_error.desc;
+                   }));
   }
 
 let optional (p : 'a parser) : 'a option parser =
   {
     run =
       (fun input ->
-        let input', result = p.run input in
-        match result with Ok x -> (input', Ok (Some x)) | Error _ -> (input', Ok None));
-  }
-
-let many_exact (n : int) (p : 'a parser) : 'a list parser =
-  {
-    run =
-      (fun input ->
-        let rec loop i xs input' =
-          if i < n then
-            let input'', result = p.run input' in
-            match result with
-            | Ok x -> loop (i + 1) (x :: xs) input''
-            | Error e -> (input'', Error e)
-          else (input', Ok (List.rev xs))
-        in
-        loop 0 [] input);
+        match p.run input with Ok (input', x) -> Ok (input', Some x) | Error _ -> Ok (input, None));
   }
 
 let many (p : 'a parser) : 'a list parser =
   {
     run =
       (fun input ->
-        let xs = ref [] in
+        let result = ref [] in
         let rec loop input =
-          let input', result = p.run input in
-          match result with
-          | Ok x ->
-              xs := x :: !xs;
+          match p.run input with
+          | Ok (input', x) ->
+              result := x :: !result;
               loop input'
           | Error _ -> input
         in
         let input' = loop input in
-        (input', Ok (!xs |> List.rev)));
+        Ok (input', !result |> List.rev));
   }
 
 let any_char : char parser =
@@ -158,15 +141,13 @@ let any_char : char parser =
     run =
       (fun input ->
         let n = String.length input.text in
-        try (input_sub 1 (n - 1) input, Ok (String.get input.text 0))
-        with Invalid_argument _ -> (input, Error "expected any character"));
+        try Ok (input_sub 1 (n - 1) input, String.get input.text 0)
+        with Invalid_argument _ -> Error { pos = input.pos; desc = "expected any character" });
   }
+
+let make_error (desc : string) : error = { desc; pos = 0 }
 
 let predicate_char (p : char -> bool) : char parser =
   any_char
-  |> bind (fun x -> if p x then wrap x else x |> Printf.sprintf "Unexpected char `%c`" |> fail)
-
-let run (p : 'a parser) (s : string) : ('a, error) result =
-  match s |> make_input |> p.run with
-  | _, Ok x -> Ok x
-  | input', Error desc -> Error { pos = input'.pos; desc }
+  |> bind (fun x ->
+         if p x then wrap x else x |> Printf.sprintf "Unexpected char `%c`" |> make_error |> fail)
