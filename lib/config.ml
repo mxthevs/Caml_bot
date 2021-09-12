cbe2f03901ff81b70768229f12489a96e026b28c
Matheus Henrique <matheuscdasilva2@hotmail.com>
2021-09-12 18:03:55 -0300

    refact(config): Infer types

--- lib/config.ml
+++ lib/config.ml
@@ -1,6 +1,6 @@
 type t = { twitch_username : string; twitch_password : string; twitch_channel : string }
 
-let read_file (file_path : string) : string =
+let read_file file_path =
   let ch = open_in file_path in
   let n = in_channel_length ch in
   let s = really_input_string ch n in
@@ -13,7 +13,7 @@ let to_string config =
   Printf.sprintf "{ twitch.username = %s; twitch.password = [REDACTED]; twitch.channel = %s }"
     config.twitch_username config.twitch_channel
 
-let from_file (file_path : string) =
+let from_file file_path =
   let update_config config pair =
     match pair with
     | [ "twitch.username"; username ] -> { config with twitch_username = username }
