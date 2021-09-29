744e27f72cd1605ac7f100e43d3767ee909d6d52
Matheus Henrique <matheuscdasilva2@hotmail.com>
2021-09-29 08:11:55 -0300

    feat: Implement message parser  #2

---
+++ lib/message.ml
@@ -0,0 +1,137 @@
+(** A type representing an IRC command, following {{: https://tools.ietf.org/html/rfc2812#section-3} RFC 2812} *)
+type command =
+  | PASS of string
+  | NICK of string
+  | USER of string list  (** see rfc *)
+  | OPER of string * string  (** name * password *)
+  | MODE of string * string  (** nick * mode string *)
+  | QUIT of string  (** quit message *)
+  | SQUIT of string * string  (** server * comment *)
+  | JOIN of string list * string list  (** channels * key list *)
+  | JOIN0  (** join 0 (parts all channels) *)
+  | PART of string list * string  (** channels * comment *)
+  | TOPIC of string * string  (** chan * topic *)
+  | NAMES of string list  (** channels *)
+  | LIST of string list  (** channels *)
+  | INVITE of string * string  (** nick * chan *)
+  | KICK of string list * string * string  (** channels * nick * comment *)
+  | PRIVMSG of string * string * string  (** target * message * user *)
+  | NOTICE of string * string  (** target * message *)
+  | PING of string * string
+  | PONG of string * string
+  | Other of string * string list  (** command name * parameters *)
+
+type t = { prefix : string option; command : command }
+
+let extract_prefix str =
+  if str <> "" && str.[0] = ':' then (
+    let prefix_length = String.index str ' ' - 1 in
+    assert (prefix_length >= 0);
+    ( Some (String.sub str 1 prefix_length),
+      String.sub str (prefix_length + 2) (String.length str - (prefix_length + 2)) ))
+  else (None, str)
+
+let extract_user_from_prefix str = str |> Parser.take_until '!'
+
+let extract_trail str =
+  try
+    let trail_start = String.index str ':' + 1 in
+    let trail_length = String.length str - trail_start in
+    let rest = if trail_start > 2 then String.sub str 0 (trail_start - 2) else "" in
+    (rest, Some (String.sub str trail_start trail_length))
+  with Not_found -> (str, None)
+
+exception ParseError of string * string
+
+let split_spaces str = Util.split ~str ~c:' '
+
+let split_comma str = Util.split ~str ~c:','
+
+let split_space1 str = Util.split1_exn ~str ~c:' '
+
+(* split parameters into tokens separated by spaces. If a trail, prefixed
+   by ':', exists, it is the last token *)
+let split_params params =
+  let s, trail = extract_trail params in
+  let tokens = split_spaces s |> List.map String.trim |> List.filter (fun s -> s <> "") in
+  match trail with None -> tokens | Some trail -> tokens @ [ trail ]
+
+let fail_ msg err = raise (ParseError (msg, err))
+
+(* expect exactly one word *)
+let expect1 msg = function [ x ] -> x | _ -> fail_ msg "expected one parameter"
+
+and expect2 msg = function [ x; y ] -> (x, y) | _ -> fail_ msg "expected two parameters"
+
+and expect1or2 msg = function
+  | [ x ] -> (x, "")
+  | [ x; y ] -> (x, y)
+  | _ -> fail_ msg "expected one or two parameters"
+
+and expect2or3 msg = function
+  | [ x; y ] -> (x, y, "")
+  | [ x; y; z ] -> (x, y, z)
+  | _ -> fail_ msg "expected one or two parameters"
+
+let parse_exn msg =
+  if String.length msg = 0 then fail_ msg "Zero-length message"
+  else
+    let prefix, rest = extract_prefix msg in
+    let command_name, params = split_space1 rest in
+    let params = split_params params in
+    let command =
+      match command_name with
+      | "PASS" -> PASS (expect1 msg params)
+      | "NICK" -> NICK (expect1 msg params)
+      | "USER" -> USER (split_spaces (expect1 msg params))
+      | "OPER" ->
+          let name, pass = expect2 msg params in
+          OPER (name, pass)
+      | "MODE" ->
+          let nick, mode = expect2 msg params in
+          MODE (nick, mode)
+      | "QUIT" -> QUIT (expect1 msg params)
+      | "JOIN" -> (
+          match params with
+          | [ "0" ] -> JOIN0
+          | [ chans ] -> JOIN (split_comma chans, [])
+          | [ chans; keys ] -> JOIN (split_comma chans, split_comma keys)
+          | _ -> fail_ msg "expected one or two parameters to JOIN")
+      | "PART" ->
+          let chans, msg = expect1or2 msg params in
+          PART (split_comma chans, msg)
+      | "TOPIC" ->
+          let chan, topic = expect1or2 msg params in
+          TOPIC (chan, topic)
+      | "NAMES" -> NAMES (split_comma (expect1 msg params))
+      | "LIST" -> LIST (split_params (expect1 msg params))
+      | "INVITE" ->
+          let nick, chan = expect2 msg params in
+          INVITE (nick, chan)
+      | "KICK" ->
+          let chans, nick, c = expect2or3 msg params in
+          KICK (split_comma chans, nick, c)
+      | "PRIVMSG" ->
+          let target, msg = expect2 msg params in
+          let user = match prefix with Some s -> extract_user_from_prefix s | None -> "" in
+          PRIVMSG (target, msg, user)
+      | "NOTICE" ->
+          let target, msg = expect2 msg params in
+          NOTICE (target, msg)
+      | "PING" ->
+          let middle, trailer = expect1or2 msg params in
+          PING (middle, trailer)
+      | "PONG" ->
+          let middle, trailer = expect1or2 msg params in
+          PONG (middle, trailer)
+      | other -> Other (other, params)
+    in
+    { prefix; command }
+
+let parse s =
+  try Result.Ok (parse_exn s) with
+  | ParseError (m, e) -> Result.Error (Printf.sprintf "failed to parse \"%s\" because: %s" m e)
+  | e ->
+      Result.Error
+        (Printf.sprintf "unexpected error trying to parse \"%s\": %s" s (Printexc.to_string e))
+
--- lib/util.ml
+++ lib/util.ml
@@ -1,1 +1,17 @@
 let list_to_option xs = match xs with x :: _ -> Some x | [] -> None
+
+let split ~str ~c =
+  let rec rev_split' ~str ~i ~c ~acc =
+    try
+      let index = String.index_from str i c in
+      let before = String.sub str i (index - i) in
+      rev_split' ~str ~c ~i:(index + 1) ~acc:(before :: acc)
+    with Not_found -> String.sub str i (String.length str - i) :: acc
+  in
+  List.rev (rev_split' ~str ~i:0 ~c ~acc:[])
+
+let split1_exn ~str ~c =
+  let index = String.index str c in
+  let before = String.sub str 0 index in
+  let after = String.sub str (index + 1) (String.length str - index - 1) in
+  (before, after)
