628852f7d8b3f12c8aac9d888e515971e74bc599
Matheus Henrique <matheuscdasilva2@hotmail.com>
2021-11-08 14:05:22 -0300

    refact: Also use lsplit2 in take_until

--- lib/parser.ml
+++ lib/parser.ml
@@ -1,15 +1,19 @@
 let skip i s = String.sub s i (String.length s - i)
 
-let take_until c s = s |> String.split_on_char c |> List.hd
+let lsplit2 str ~on =
+  let open String in
+  match index_opt str on with
+  | Some pos -> Some (sub str 0 pos, sub str (pos + 1) (length str - pos - 1))
+  | None -> None
+
+let take_until_exn c s =
+  match lsplit2 s ~on:c with
+  | Some (hd, _) -> hd
+  | None -> Printf.sprintf "%s has no character %c" s c |> failwith
+
+let take_until c s = try take_until_exn c s with Failure _ -> ""
 
 let take_after_exn c s =
-  let lsplit2 str ~on =
-    let open String in
-    match index_opt str on with
-    | Some pos -> Some (sub str 0 pos, sub str (pos + 1) (length str - pos - 1))
-    | None -> None
-  in
-
   match lsplit2 s ~on:c with
   | Some (_, rest) -> rest
   | None -> Printf.sprintf "%s has no character %c" s c |> failwith
