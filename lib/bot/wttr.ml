e54164d7e7070d00d4109ed0100a02414e4a9a66
Matheus Henrique <matheuscdasilva2@hotmail.com>
2022-01-13 17:33:06 -0300

    feat: use CoHttp

--- lib/bot/wttr.ml
+++ lib/bot/wttr.ml
@@ -2,6 +2,4 @@ let handle (location, sender) =
   let format = {|%l: %c 🌡️%t\n|} in
   let url = "https://wttr.in/" ^ location ^ "?format=" ^ format in
 
-  match Http.get_sync url with
-  | Ok body -> body
-  | Error _ -> "@" ^ sender ^ ", esse lugar nem existe cara"
+  Http.get_sync url
