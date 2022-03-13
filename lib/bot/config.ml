3c445e0edbffc4b0bc334b3b81e4335d2819ef12
Matheus Henrique <matheuscdasilva2@hotmail.com>
2022-03-13 15:45:46 -0300

    refact: Remove `Bot.Config` module

--- lib/bot/config.ml
+++
@@ -1,9 +0,0 @@
-module Database = struct
-  let connection_url =
-    let username = Sys.getenv "USERNAME" in
-    let password = Sys.getenv "PASSWORD" in
-    let database = Sys.getenv "DATABASE" in
-    let port = Sys.getenv "PORT" in
-
-    Printf.sprintf "postgresql://%s:%s@localhost:%s/%s" username password port database
-end
