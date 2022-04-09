00fd61761d8942168985d66804d7fc84c68dc563
Matheus Henrique <matheuscdasilva2@hotmail.com>
2022-04-09 14:17:22 -0300

    feat: Update commands gist

--- src/http.ml
+++ src/http.ml
@@ -38,3 +38,24 @@ let post_sync ?headers url ~body =
   in
 
   Lwt_main.run (post ())
+
+let patch_sync ?headers url ~body =
+  let patch () =
+    Client.patch (Uri.of_string url)
+      ~headers:
+        (match headers with
+        | Some h -> Cohttp.Header.of_list h
+        | None -> Cohttp.Header.init ())
+      ~body:(Cohttp_lwt.Body.of_string body)
+    >>= fun (resp, body) ->
+    let code = resp |> Response.status |> Code.code_of_status in
+
+    if Code.is_success code then
+      body |> Cohttp_lwt.Body.to_string >|= fun body -> Ok body
+    else
+      body |> Cohttp_lwt.Body.to_string >|= fun body ->
+      [%log err "%s" body];
+      Error (`Body body)
+  in
+
+  Lwt_main.run (patch ())
